78f37f62719297c35de883a895c18f3cea9fb3f8
cmykfan <31507205+cmykfan@users.noreply.github.com>
2017-11-10 15:17:04 -0500

    Push JS files
    Push JS files

---
+++ scripts/GenesisQuiz.js
@@ -0,0 +1,25 @@
+<html>
+   <head>
+   
+      <script type="text/javascript">
+         <!--
+            function Warn() {
+               alert ("This is a warning message!");
+               document.write ("This is a warning message!");
+            }
+         //-->
+      </script>
+      
+   </head>
+   <body>
+      <p>Click the following button to see the result: </p>
+      
+      <form>
+         <input type="button" value="Click Me" onclick="Warn();" />
+      </form>
+      
+   </body>
+</html>
+
+
+widow.alert("This funtionality not yet implimented.")
