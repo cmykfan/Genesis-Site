cff1ba5baec56c470b080f5450deedd471f94897
cmykfan <31507205+cmykfan@users.noreply.github.com>
2017-11-12 21:29:37 -0500

    Push corrected js files.
    Push corrected js files.

---
+++ scripts/aboutPeter.js
@@ -0,0 +1,9 @@
+<button onclick="myFunction()">Peter Gabriel Era: 1967-1975</button>
+<script>
+function myFunction() {
+    alert("This era, which saw Peter Gabriel as the singer and front person for the group, was considered the most artistic and progressive in the group's career.  Peter would use costumes and complex cinematics in order to tell a story while he was singing.");
+}
+
+
+
+</script>
---
+++ scripts/genesisQuiz.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+         <!--
+            function Warn() {
+               alert ("This functionality not yet implimented");
+               document.write ("This functionality not yet implimented");
+            }
+         //-->
+      </script>
+      
