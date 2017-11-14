0755ab911dfa3d35829a806a6a54f300474c1e2e
cmykfan <31507205+cmykfan@users.noreply.github.com>
2017-11-14 10:47:31 -0500

    Push changes in js file.
    Push changes in js file.

---
+++ scripts/aboutPeter.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    alert("This era, which saw Peter Gabriel as the singer and front person for the group, was considered the most artistic and progressive in the group's career.  Peter would use costumes and complex cinematics in order to tell a story while he was singing.  See album detail links at the bottom of the webpage");
+}
+
+
